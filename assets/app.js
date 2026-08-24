3319194b62c9bbb21bd74c60eaf0dfb6416216d2
softeasyhk <iamdpeasy@gmail.com>
2026-08-24 18:09:49 +0100

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -54,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    chat_id: "@Efccsg",
+                    chat_id: "8735261618",
                     text: message
                 })
             }
